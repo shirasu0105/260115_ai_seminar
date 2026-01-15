3a3b1004946bee3b6382f22df783dd5992357d53
HayatoSuzuki <hayato.suzuki@omron.com>
2026-01-15 19:15:36 +0900

    ○完了 ・環境構築 ・サンプルコードの実行 ・ワインの銘柄判定モデルスクリプトの作成
    ○次回
    モデルの再現性確認
    　→設定したシード値により、たまたま最高スコアを出した可能性をテスト
    　→再現性なしの場合、学習フローを改善する

---
+++ project/02_wine/main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+
+#csvファイル読み込み
+df = pd.read_csv(r'C:\Work\Python\260115_ai_seminar\project\02_wine\wine_dataset.csv')
+
+#特徴量とラベルを分解
+X = df.drop(columns=['target'])
+y = df['target']
+
+# データを訓練用とテスト用に分割
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=40)
+
+# モデル生成
+model = RandomForestClassifier(n_estimators=100, random_state=40)
+model.fit(X_train, y_train)
+
+# テストデータ予測作成
+y_pred = model.predict(X_test)
+
+# 評価結果表示
+print("Accuracy Score:", accuracy_score(y_test,y_pred))
+print("\nClassification report:")
+print(classification_report(y_test, y_pred))
